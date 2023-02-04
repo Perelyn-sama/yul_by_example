d7bf86b6a9062609971302ff0c01ed2434638295
Perelyn <pisckid344@gmail.com>
2023-02-04 23:04:10 +0100

    add counter

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,32 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+// https://solidity-by-example.org/first-app
 contract Counter {
-    uint256 public number;
+    // slot 0
+    uint256 public count;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    // Function to get the current count
+    function get() public view returns (uint256) {
+        uint256 count;
+        assembly {
+            count := sload(0)
+        }
+        return count;
     }
 
-    function increment() public {
-        number++;
+    // Function to increment count by 1
+    function inc() public {
+        assembly {
+            sstore(0, add(sload(0), 1))
+        }
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        // yul does not prevent underflow/overflow so this will work
+        assembly {
+            sstore(0, sub(sload(0), 1))
+        }
     }
 }
