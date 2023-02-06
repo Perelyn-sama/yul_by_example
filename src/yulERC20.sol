140891efd1d7115272253a3f524adcaa4bc7c017
Perelyn <pisckid344@gmail.com>
2023-02-06 23:13:47 +0100

    fixed name function error

--- src/yulERC20.sol
+++ src/yulERC20.sol
@@ -3,39 +3,12 @@ pragma solidity ^0.8.17;
 
 // used in the name() function
 // "Yul Token"
-bytes32 constant nameLength = 0x9000000000000000000000000000000000000000000000000000000000000000;
+bytes32 constant nameLength = 0x0000000000000000000000000000000000000000000000000000000000000009;
 bytes32 constant nameData = 0x59756c20546f6b656e0000000000000000000000000000000000000000000000;
 
 // used in the symbol() function
 // "YUL"
-bytes32 constant symbolLength = 0x9000000000000000000000000000000000000000000000000000000000000000;
-bytes32 constant symbolData = 0x59554c0000000000000000000000000000000000000000000000000000000000;
-
-// bytes4(keccak256("InsufficientBalance()")) 0xf4d678b8
-bytes32 constant InsufficientBalanceSelector = 0xf4d678b800000000000000000000000000000000000000000000000000000000;
-
-// bytes4(keccak256("InsufficientAllowance(address, address)")) 0xf180d8f9
-bytes32 constant InsufficientAllowanceSelector = 0xf180d8f900000000000000000000000000000000000000000000000000000000;
-
-    error InsufficientBalance();
-    error InsufficientAllowance(address owner, address spender);
-
-// @title Yul ERC20
-// @author Some random Dude
-
-contract YulERC20 {
-
-}// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
-
-// used in the name() function
-// "Yul Token"
-bytes32 constant nameLength = 0x9000000000000000000000000000000000000000000000000000000000000000;
-bytes32 constant nameData = 0x59756c20546f6b656e0000000000000000000000000000000000000000000000;
-
-// used in the symbol() function
-// "YUL"
-bytes32 constant symbolLength = 0x9000000000000000000000000000000000000000000000000000000000000000;
+bytes32 constant symbolLength = 0x0000000000000000000000000000000000000000000000000000000000000003;
 bytes32 constant symbolData = 0x59554c0000000000000000000000000000000000000000000000000000000000;
 
 // bytes4(keccak256("InsufficientBalance()")) 0xf4d678b8
