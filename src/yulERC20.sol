19cce5691d588e32fe8be7a7edc359b085b0683e
Perelyn <pisckid344@gmail.com>
2023-02-11 23:56:55 +0100

    add maxUint256 variable

--- src/yulERC20.sol
+++ src/yulERC20.sol
@@ -20,6 +20,9 @@ bytes32 constant InsufficientAllowanceSelector = 0xf180d8f9000000000000000000000
 error InsufficientBalance();
 error InsufficientAllowance(address owner, address spender);
 
+//max uint256 value, mints everything to the deployer
+uint256 constant maxUint256 = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
 bytes32 constant transferHash = 0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef;
 bytes32 constant approvalHash = 0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925;
 
