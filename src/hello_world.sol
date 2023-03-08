95b790a3bb34b51fee405ab4decb5ac06d01428b
Tanim0la <36541366+tanim0la@users.noreply.github.com>
2023-03-08 08:17:22 +0100

    Update hello_world.sol

--- src/hello_world.sol
+++ src/hello_world.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.8.17;
 // https://solidity-by-example.org/hello-world
 contract HelloWorld {
     function Greet() external pure returns (string memory) {
-        bytes32 greet;
         assembly {
-            greet := "Hello World!"
+            // Assign the string to var `greet`
+            let greet := "Hello World!"
+            // Store the string offset in mem[0x00]
+            mstore(0x00, 0x20)
+            // Store the length of the string in mem[0x20]
+            mstore(0x20, 0x0c)
+            // Store the string in mem[0x40]
+            mstore(0x40, greet)
+            // Returns the bytes from mem[offset to offset+size]
+            return(0x00,0x60)
         }
-        return string(abi.encode(greet));
     }
 }
