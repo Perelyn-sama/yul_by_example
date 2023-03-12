36730f867a3ce92f1a39bffd16082a2a158f7e5f
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2023-03-12 23:55:26 +0100

    update SendEther.sol

---
+++ src/SendEther.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract SendEther {
+
+    constructor() payable {}
+    
+    function transfer(uint256 amount, address to) external {
+        assembly {
+            let s := call(gas(), to, amount, 0, 0, 0, 0)
+            if iszero(s) {
+                revert(0, 0)
+            }
+        }
+    }
+
+}
