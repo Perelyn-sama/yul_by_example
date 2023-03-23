241e2ac0ac83c954e4e72094d46cc60451f07cc9
Perelyn <pisckid344@gmail.com>
2023-03-23 14:56:56 +0100

    Fix contract path in tests

--- test/EtherWallet.t.sol
+++ test/EtherWallet.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Ether Wallet.sol";
+import "../src/EtherWallet.sol";
 
 contract EtherWalletTest is Test {
 
--- test/SendEther.t.sol
+++ test/SendEther.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Send Ether.sol";
+import "../src/SendEther.sol";
 
 contract SendEtherTest is Test {
 
