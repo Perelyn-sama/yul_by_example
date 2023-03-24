6e5b7fb328439b8a2fc61056a939eeb4784ff15b
Perelyn <pisckid344@gmail.com>
2023-03-24 18:41:38 +0100

    Add test for HelloWorld.sol

---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/HelloWorld.sol";
+
+contract HelloWorldTest is Test {
+    HelloWorld public helloWorld;
+
+    function setUp() public {
+        helloWorld = new HelloWorld();
+    }
+
+    function testGreet() public {
+        assertEq(helloWorld.Greet(), "Hello World!");
+    }
+}
